d09423f2d71dc664c948fc476fd61d144d2911ef
theo <lesgooo1993@proton.me>
2023-06-24 23:38:19 +0530

    adapter-static; prerender; trailingslash

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,3 +3,6 @@ export function load({ url }) {
         url: url.pathname,
     }
 }
+
+export const prerender = true;
+export const trailingSlash = 'always';
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    },
 	preprocess: vitePreprocess()
 };
 
